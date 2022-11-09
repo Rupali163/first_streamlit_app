bcd2ab3bddc22b9febe5bdf2130307e7659c59d3
Rupali <106401434+Rupali163@users.noreply.github.com>
2022-11-09 17:16:41 +0530

    Update streamlit_app.py
    using pandas lib we add s3 bucket csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('avcado')
 streamlit.text('kala, splinach')
 streamlit.text('almond milk')
 streamlit.text('toasted Bread')
+
+import pandas  as pd
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
