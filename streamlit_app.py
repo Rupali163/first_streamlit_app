91e493205aa7785ebb78cc46275c2c2504c9b8d9
Rupali <106401434+Rupali163@users.noreply.github.com>
2022-11-09 17:06:13 +0530

    Update streamlit_app.py
    fixed_typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('New line is added')
 streamlit.title('Updating streamlite data through this app!!')
+streamlit.title('Breakfast menu ....')
+streamlit.title('coeffe, sandwich,,fries!!')
